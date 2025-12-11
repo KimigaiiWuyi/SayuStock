d961b50b20258eb8b2e404f270bd4382eb0042e4
KimigaiiWuyi <444835641@qq.com>
2025-12-12 05:32:22 +0800

    🐛 修复`概念云图` (#9)

--- SayuStock/utils/stock/request.py
+++ SayuStock/utils/stock/request.py
@@ -448,6 +448,7 @@ async def stock_request(
 
     if url.startswith(
         (
+            "https://quote.eastmoney.com/center/api/sidemenu_new.json",
             "https://quote.eastmoney.com/stockhotmap/api/getquotedata",
             "https://push2his.eastmoney.com",
             "https://quotederivates.eastmoney.com",
